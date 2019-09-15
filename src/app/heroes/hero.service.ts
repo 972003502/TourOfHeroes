0a4a9a8bb2973f951a4430e4c5fc815c6daf97b1
Avalon <972003502@qq.com>
2019-09-15 20:22:50 +0800

    '2019.9.15'

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -67,7 +67,6 @@ export class HeroService {
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
